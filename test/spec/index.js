731a6c2bbc5eca9179c04e8ff3f92f12cd4a41bb
Anton <anton@adc.sh>
2018-05-11 22:50:46 +0100

    update tests

--- test/spec/index.js
+++ test/spec/index.js
@@ -1,33 +1,33 @@
 import context, { SnapshotContext } from 'snapshot-context' // eslint-disable-line no-unused-vars
-import erte from '../../src'
 import { resolve } from 'path'
+import erte from '../../src'
 
 const SNAPSHOT_DIR = resolve(__dirname, '../snapshots')
 
 /** @type {Object.<string, (ctx: SnapshotContext)>} */
-const t = {
+const T = {
   context,
-  async 'returns a string with extra text highlighted with green'(ctx) {
-    ctx.setDir(SNAPSHOT_DIR)
+  async 'returns a string with extra text highlighted with green'({ setDir, test }) {
+    setDir(SNAPSHOT_DIR)
     const s = 'test'
     const t = 'test extra'
     const res = erte(s, t)
-    await ctx.test('extra.txt', res)
+    await test('extra.txt', res)
   },
-  async 'returns a string with missing text highlighted with red'(ctx) {
-    ctx.setDir(SNAPSHOT_DIR)
+  async 'returns a string with missing text highlighted with red'({ setDir, test }) {
+    setDir(SNAPSHOT_DIR)
     const s = 'test extra'
     const t = 'test'
     const res = erte(s, t)
-    await ctx.test('missing.txt', res)
+    await test('missing.txt', res)
   },
-  async 'returns a string in grey without differences'(ctx) {
-    ctx.setDir(SNAPSHOT_DIR)
-    const s = 'test extra'
+  async 'returns a string in grey without differences'({ setDir, test }) {
+    setDir(SNAPSHOT_DIR)
+    const s = 'test'
     const t = 'test'
     const res = erte(s, t)
-    await ctx.test('missing.txt', res)
+    await test('same.txt', res)
   },
 }
 
-export default t
+export default T
